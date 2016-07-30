953c8a819a3a578985487ca0e87c92376cf5d76e
awe <albertsagi97@gmail.com>
2016-07-30 12:19:46 +0700

    my first work 1

---
+++ src/lombaurut/Lombaurut.java
@@ -0,0 +1,53 @@
+package lombaurut;
+
+import com.mkdika.Xutil;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Lombaurut {
+        @SuppressWarnings("empty-statement")
+    public static void main(String[] args) throws IOException {
+
+        // Baca text file, masukan isi text file kedalam variable array.       
+        int[] x = Xutil.readTxtFileToArray("random.dat");
+        if (x == null || x.length <= 0) {
+            System.out.println("Error read text file. try again.");
+            return;
+        }else {
+            System.out.println("Array Length: " + x.length);
+        }
+        
+        long start = Xutil.getTime();
+        // BEGIN - YOUR CODE HERE
+        // =====================================================================
+
+        // Baseline Example - Used Bubble Sort Algorithm
+            int a,b,c,d;  
+            int temp = 0;
+            d = x.length;
+            for(b = 1; b <x.length-1; b++){
+                c = x[b];
+                for(a = b-1; a>=0 && (x[a]<c); a --){
+                    x[a+1] = x[a];
+                 }
+                x[a+1] = c;
+                 }
+                 Xutil.writeTxtFileFromArray(x, "sorted.dat");      
+                 int[] y = Xutil.readTxtFileToArray("sorted.dat");
+         
+            for(b = (x.length)-1;b > x.length/2;b--){
+                
+                y[b] = temp;
+                y[b] = y[(d-b)-1];
+                y[(d-b)-1] = temp;  
+                }
+             Xutil.writeTxtFileFromArray(y, "sorted.dat");
+        // =====================================================================
+        // END
+        long end = Xutil.getTime();
+
+        System.out.println("Total execution time (nano sec): " + Xutil.executionTime(start, end));       
+        Xutil.writeTxtFileFromArray(x, "sorted.dat");
+    }
+
+}
